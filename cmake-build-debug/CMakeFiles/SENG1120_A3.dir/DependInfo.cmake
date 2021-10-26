2acca81df0fc2744ef5260194046ea87db9fd8ca
Jordan14E <92713486+Jordan14E@users.noreply.github.com>
2021-10-26 12:00:43 +1100

    starting

---
+++ cmake-build-debug/CMakeFiles/SENG1120_A3.dir/DependInfo.cmake
@@ -0,0 +1,30 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/cygdrive/c/GitHub/SENG1120_A3/MechPart.cpp" "/cygdrive/c/GitHub/SENG1120_A3/cmake-build-debug/CMakeFiles/SENG1120_A3.dir/MechPart.cpp.o"
+  "/cygdrive/c/GitHub/SENG1120_A3/TreeHashTableDemo.cpp" "/cygdrive/c/GitHub/SENG1120_A3/cmake-build-debug/CMakeFiles/SENG1120_A3.dir/TreeHashTableDemo.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "../."
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ cmake-build-debug/CMakeFiles/SENG1120_A3.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/SENG1120_A3.dir/MechPart.cpp.o"
+  "CMakeFiles/SENG1120_A3.dir/TreeHashTableDemo.cpp.o"
+  "SENG1120_A3.exe"
+  "SENG1120_A3.pdb"
+  "libSENG1120_A3.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/SENG1120_A3.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
