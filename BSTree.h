666949ee9e4b84498ae8588f310358ab31ee7d72
Jordan14E <92713486+Jordan14E@users.noreply.github.com>
2021-10-26 12:12:38 +1100

    setting up BSTree

--- BSTree.h
+++ BSTree.h
@@ -5,10 +5,32 @@
 #ifndef SENG1120_A3_BSTREE_H
 #define SENG1120_A3_BSTREE_H
 
+#include "string"
 
+template <typename Type>
 class BSTree {
+public:
+
+    void add(){};
+
+    void remove(){};
+
+    int calculateParts(){};
+
+    int calculateInventory(){};
+
+    int calculateLessThan(){};
+
+private:
+
 
 };
+
+template <typename Type>
+std::ostream& operator << (std::ostream& out, BSTree<Type> &tree){
+    return out;
+};
+
 
 
 #endif //SENG1120_A3_BSTREE_H
